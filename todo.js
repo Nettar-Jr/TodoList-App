649d0c21149ca2cff203a264ef46e77a3eea6f49
Alabi Abdulmumuni <42644248+Nettar-Jr@users.noreply.github.com>
2019-05-12 20:43:38 +0100

    Revert "Develop"

--- todo.js
+++ todo.js
@@ -1,66 +1,42 @@
-
-//click add to create items and append to list
+function newTaskOnKeyPress(){
+    newTaskOnClick;
+}
 
 function newTaskOnClick (){
-
-	//create item, delete button and checkbox
-
-	var todoList = document.getElementById('list');
+    
+//click add to create items and append to list
 
 	var todoItem = document.createElement('li');   
 	var deleteButton = document.createElement('span');
-	var checkBox = document.createElement('input'); 
+	var checkBox = document.createElement('input');
 
 	checkBox.className = "check";
 	checkBox.type = "checkbox";
 	deleteButton.innerText = "x";
 	deleteButton.className = "Remove";
-
-
-	//append and insert text node, delete button  and check box
-
+	 
 	var inputValue = document.getElementById('input').value;
 	var txt = document.createTextNode(inputValue);
 	todoItem.appendChild(txt);
 	todoItem.appendChild(deleteButton);
 	todoItem.insertBefore(checkBox, txt);
 	 
-
-	// if input is blank the if statement will ask user to input task
-
-		if (inputValue === ''){
-			alert ('please enter a task');
-		}
-		else {
-			todoList.appendChild(todoItem);
-
-
-			//give unique id to each list item.
-
-			var grabList = document.querySelectorAll('li');
-			for (var i = 0; i < grabList.length; i++){
-				var grabLi = grabList[i];
-			}
-			grabLi.id = 'item' + i;
-		}
-		document.getElementById('input').value;
-		storeItems();
-	 
-		
+	if (inputValue === ''){
+		alert ('please enter a task');
+	}
+	else {
+		document.getElementById('list').appendChild(todoItem);
+	}
+    document.getElementById('input').value;
+     
 	// hide list when delete button is clicked
-	deleteButton.onclick = function(event){
-		event.stopPropagation();
-		todoItem.parentNode.removeChild(todoItem);	
-		
-		storeItems();
+	 
+	deleteButton.onclick = function(){
+		todoItem.style.display = "none";
 	}
 		 
-
 	//checked box create line-through effect on list.
-	checkBox.ondblclick = function(event){event.stopPropagation();} // this stops the checkbox from perfprming and action on double click.
-	
-	checkBox.onclick = function(event){
-		event.stopPropagation();
+	checkBox.onclick = function(){
 		if (todoItem.style.textDecoration === "line-through"){
 			todoItem.style.textDecoration = "none";
 		}
@@ -68,37 +44,5 @@ function newTaskOnClick (){
 			todoItem.style.textDecoration = "line-through";
 		}
 	}
-
-	// save list to system local storage
-	var storedIitems;
-	function storeItems(){
-		storedIitems = localStorage.setItem("task", list.innerHTML)
-	}
-
-	//get list from local storage
-	function getItems(){
-		storedIitems = JSON.parse(localStorage.getItem("task"));
-		if (!list){
-			list = [];
-		}
-		else{
-		
-		list.innerHTML = storedIitems;
-		} 
-	}
-	//getItems();
-
-	//double click item to add nested list
-	
-
-	// click to edit your task
-	todoItem.onclick = function editItem(){
-
-		var itemEdit = document.createElement("input");
-		itemEdit.type = "text";
-
-		todoItem.textContent = (itemEdit.value);
-
-	}
-	
+ 
 }
